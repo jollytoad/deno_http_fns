256d6b079c2ddb97557097d62c5e05f39338e626
Mark Gibson <mgibson@adaptavist.com>
2023-07-14 16:22:58 +0100

    add fresh path mapper to dynamic route example

--- examples/dynamic_route.ts
+++ examples/dynamic_route.ts
@@ -1,10 +1,12 @@
 import { withFallback } from "https://deno.land/x/http_fns/fallback.ts";
 import { dynamicRoute } from "https://deno.land/x/http_fns/dynamic.ts";
+import freshPathMapper from "https://deno.land/x/http_fns/fresh/path_mapper.ts";
 
 Deno.serve(withFallback(
   dynamicRoute({
     pattern: "/",
     fileRootUrl: import.meta.resolve("./routes"),
+    pathMapper: freshPathMapper,
     eagerness: "startup",
     verbose: true,
   }),
