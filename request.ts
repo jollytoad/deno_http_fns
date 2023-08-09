6426013bbaa18d939e2450dfdb07941f58b392ec
Mark Gibson <mgibson@adaptavist.com>
2023-08-09 14:43:48 +0100

    Remove deprecated request/response modules

--- request.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @module
- * @deprecated use `request/*.ts` modules directly instead.
- */
-
-export * from "./request/required_header.ts";
-export * from "./request/url_header.ts";
-export * from "./request/body_as_object.ts";
-export * from "./request/search_values.ts";
